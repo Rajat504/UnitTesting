b8edcee88961e73b28b690d627829133e0d78fec
Rajat504 <rajatravindar504@gmail.com>
2020-03-09 14:50:32 +0530

    [Rajat] add.have added userregistration case

---
+++ XUnitRegex/XUnitRegex/Userregistration.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace XUnitRegex
+{
+    class Userregistration
+    {
+        public static bool FirstName(string name)
+        {
+            var nameregexpattern = "^([a-zA-Z]*){1,15}$";
+            Regex pattern = new Regex(nameregexpattern);
+            if(pattern.IsMatch(name))
+            {
+                return true;
+
+            }
+
+            return false;
+        }
+        public static bool LastName(string name)
+        {
+            var nameregexpattern = "^([a-zA-Z]*){1,10}$";
+            Regex pattern = new Regex(nameregexpattern);
+            if (pattern.IsMatch(name))
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+        public static bool email(string name)
+        {
+            var emailpattern= "^[A-Za-z0-9/.]{6,}@[a-zA-Z]{4,}.(com|in|co.in)$"; 
+            Regex pattern = new Regex(emailpattern);
+            if (pattern.IsMatch(name))
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+        public static bool password(string name)
+        {
+            var password = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[/-_!@#$%^&*])(?=.{8,14})";
+            Regex pattern = new Regex(password);
+            if (pattern.IsMatch(name))
+            {
+                return true;
+
+            }
+            else
+                return false;
+        }
+        public static bool contact(string name)
+        {
+            var contact = "^[0-9]{10}$";
+            Regex pattern = new Regex(contact);
+            if (pattern.IsMatch(name))
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+
+    }
+}
